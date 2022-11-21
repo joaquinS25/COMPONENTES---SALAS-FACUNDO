6c869112e876ba1661142606d5c4b7f7702408c8
joaquinS25 <112294606+joaquinS25@users.noreply.github.com>
2022-11-21 14:27:21 -0500

    AGREGANDO COMPONENTES AL PTOYECTO

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BarraDeNavegacionComponent } from './barra-de-navegacion/barra-de-navegacion.component';
+import { ContenidoComponent } from './contenido/contenido.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraDeNavegacionComponent,
+    ContenidoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
